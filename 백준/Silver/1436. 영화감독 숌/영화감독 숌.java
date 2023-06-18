36262f63ab18eb6ab74e0cea6b45b596aae43568
정승규 <43807509+jsk41755@users.noreply.github.com>
2023-06-18 18:39:23 +0900

    [Silver V] Title: 영화감독 숌, Time: 296 ms, Memory: 87080 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Main {
+ 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int num = 666;
+		int count = 1;
+		
+		while(count != N) {
+			num++;
+			if(String.valueOf(num).contains("666")) {
+				count++;
+			}
+		}
+		
+		System.out.println(num);
+	}
+}
