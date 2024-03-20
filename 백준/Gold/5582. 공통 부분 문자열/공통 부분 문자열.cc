b63508c6abf7bbbc67dd8b5a4e32830b67159f17
정승규 <43807509+jsk41755@users.noreply.github.com>
2024-03-20 23:53:09 +0900

    [Gold V] Title: 공통 부분 문자열, Time: 76 ms, Memory: 64556 KB -BaekjoonHub

--- 백준/Gold/5582. 공통 부분 문자열/공통 부분 문자열.cc
+++ 백준/Gold/5582. 공통 부분 문자열/공통 부분 문자열.cc
@@ -15,9 +15,9 @@ int main()
     cin >> a >> b;
 
     int answer = 0;
-    for (int i = 1; i <= a.size(); i++) {
-        for (int j = 1; j <= b.size(); j++) {
-            if (a[i-1] == b[j-1]) {
+    for (int i = 1; i <= b.size(); i++) {
+        for (int j = 1; j <= a.size(); j++) {
+            if (b[i-1] == a[j-1]) {
                 sol[i][j] += sol[i - 1][j - 1] + 1;
             }
             answer = max(answer, sol[i][j]);
