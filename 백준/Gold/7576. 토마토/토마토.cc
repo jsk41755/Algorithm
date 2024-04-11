c4decab3756098014b8b50e54ce40576ac8ab6a6
정승규 <43807509+jsk41755@users.noreply.github.com>
2024-04-11 21:05:47 +0900

    [Gold V] Title: 토마토, Time: 88 ms, Memory: 14128 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.cc
+++ 백준/Gold/7576. 토마토/토마토.cc
@@ -1,55 +1,71 @@
-#include <cstdio>
-#include <queue>
-using namespace std;
-int a[1000][1000];
-int d[1000][1000];
-int dx[] = {0,0,1,-1};
-int dy[] = {1,-1,0,0};
-int main() {
-    int n,m;
-    scanf("%d %d",&m,&n);
-    queue<pair<int,int>> q;
-    for (int i=0; i<n; i++) {
-        for (int j=0; j<m; j++) {
-            scanf("%d",&a[i][j]);
-            d[i][j] = -1;
-            if (a[i][j] == 1) {
-                q.push(make_pair(i,j));
-                d[i][j] = 0;
-            }
-        }
-    }
-    while (!q.empty()) {
-        int x = q.front().first;
-        int y = q.front().second;
-        q.pop();
-        for (int k=0; k<4; k++) {
-            int nx = x+dx[k];
-            int ny = y+dy[k];
-            if (0 <= nx && nx < n && 0 <= ny && ny < m) {
-                if (a[nx][ny] == 0 && d[nx][ny] == -1) {
-                    d[nx][ny] = d[x][y] + 1;
-                    q.push(make_pair(nx,ny));
-                }
-            }
-        }
-    }
-    int ans = 0;
-    for (int i=0; i<n; i++) {
-        for (int j=0; j<m; j++) {
-            if (ans < d[i][j]) {
-                ans = d[i][j];
-            }
-        }
-    }
-    for (int i=0; i<n; i++) {
-        for (int j=0; j<m; j++) {
-            if (a[i][j] == 0 && d[i][j] == -1) {
-                ans = -1;
-            }
-        }
-    }
+#include<iostream>
+#include<queue>
 
-    printf("%d\n",ans);
-    return 0;
+using namespace std;
+
+int m, n;
+int a[1001][1001];
+int d[1001][1001];
+
+int dx[4] = { 1, -1, 0, 0 };
+int dy[4] = { 0, 0, 1, -1 };
+int cnt = 0;
+
+bool isGone = false;
+
+queue<pair<int, int>> q;
+
+int main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> m >> n;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> a[i][j];
+			d[i][j] = -1;
+			if (a[i][j] == 1) {
+				q.push({ i, j });
+				d[i][j] = 0;
+			}
+		}
+	}
+
+	while (!q.empty()) {
+		int x = q.front().first;
+		int y = q.front().second;
+		
+		q.pop();
+
+		for (int i = 0; i < 4; i++) {
+			int ax = x + dx[i];
+			int ay = y + dy[i];
+
+			if (ax >= 0 && ax < n && ay >= 0 && ay < m && a[ax][ay] == 0 && d[ax][ay] == -1) {
+				q.push({ ax, ay });
+				d[ax][ay] = d[x][y] + 1;
+			}
+		}
+
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (cnt < d[i][j]) {
+				cnt = d[i][j];
+			}
+		}
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (a[i][j] == 0 && d[i][j] == -1) {
+				cout << -1;
+				return 0;
+			}
+		}
+	}
+
+	cout << cnt;
 }
