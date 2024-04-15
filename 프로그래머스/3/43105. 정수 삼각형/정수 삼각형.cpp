db9e37a0bbef6c46f91e4e210953e95e158797b9
정승규 <43807509+jsk41755@users.noreply.github.com>
2024-04-15 22:38:19 +0900

    [level 3] Title: 정수 삼각형, Time: 1.52 ms, Memory: 9.84 MB -BaekjoonHub

---
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(vector<vector<int>> triangle) {
+    int answer = 0;
+    
+    int a[501][501];
+    int d[501][501];
+    
+    for(int i=0; i<triangle.size(); i++){
+        for(int j=0; j<triangle[i].size(); j++){
+            a[i+1][j+1] = triangle[i][j];
+        }
+    }
+    
+    for(int i=1; i<triangle.size()+1; i++){
+        for(int j=1; j<=i; j++){
+            d[i][j] = a[i][j] + max(d[i-1][j-1], d[i-1][j]);
+        }
+    }
+    
+    for(int i=1; i<triangle.size()+1; i++){
+        answer = max(answer, d[triangle.size()][i]);
+    }
+    
+    return answer;
+}
