b5cc8bbb95034d8a1b8febf4ddb5c062aca1c670
정승규 <43807509+jsk41755@users.noreply.github.com>
2024-04-18 18:33:40 +0900

    [Gold IV] Title: 생일 선물, Time: 32 ms, Memory: 3480 KB -BaekjoonHub

---
+++ 백준/Gold/12892. 생일 선물/생일 선물.cc
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n, d;
+
+	cin >> n >> d;
+
+	vector<pair<long, long>> a(n+1);
+
+	for (int i = 1; i <= n; i++) {
+		int p, v;
+		cin >> p >> v;
+		a[i].first = p;
+		a[i].second = v;
+	}
+
+	sort(a.begin() + 1, a.end());
+	int l, r;
+	l = r = 1;
+
+	long long temp = 0;
+	long long sol = 0;
+	while (l <= r && r <= n) {
+		if (a[r].first - a[l].first < d) {
+			temp += a[r].second;
+			r++;
+		}
+		else {
+			temp -= a[l].second;
+			l++;
+		}
+		sol = max(sol, temp);
+	}
+
+	cout << sol;
+
+	return 0;
+}
