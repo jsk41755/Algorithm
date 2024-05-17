3e423f796eed62161ad6e8f800800791e65defe6
정승규 <43807509+jsk41755@users.noreply.github.com>
2024-05-17 15:55:09 +0900

    [Silver I] Title: Z, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Silver/1074. Z/Z.cc
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int n, r, c;
+int ans = 0;
+
+void dc(int x, int y, int size) {
+    if (c == x && r == y) {
+        cout << ans;
+        return;
+    }
+    else if (c < x + size && r < y + size && c >= x && r >= y) {
+        dc(x, y, size / 2);
+        dc(x + size / 2, y, size / 2);
+        dc(x, y + size / 2, size / 2);
+        dc(x + size / 2, y + size / 2, size / 2);
+    }
+    else {
+        ans += size * size;
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> n >> r >> c;
+
+    dc(0, 0, pow(2, n));
+
+    return 0;
+}
