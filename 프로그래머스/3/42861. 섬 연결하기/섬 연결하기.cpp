54c7b34e3082464411a52e39a133f72b22ab19d1
정승규 <43807509+jsk41755@users.noreply.github.com>
2024-09-17 01:04:12 +0900

    [level 3] Title: 섬 연결하기, Time: 0.04 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.cpp
@@ -0,0 +1,49 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> arr;
+
+int Find(int i){
+    if(arr[i] == i) return i;
+    else return arr[i] = Find(arr[i]);
+}
+
+void Union(int a, int b){
+    int aRoot = Find(a);
+    int bRoot = Find(b);
+    
+    arr[aRoot] = bRoot;
+}
+
+int solution(int n, vector<vector<int>> costs) {
+    int answer = 0;
+    
+    sort(costs.begin(), costs.end(), [](const vector<int> a, const vector<int> b) {
+        return a[2] < b[2];
+    });
+    
+    arr.resize(n+1);
+    
+    for(int i=0; i<n; i++){
+        arr[i] = i;
+    }
+    
+    int edges = 0;
+    
+    for(int i=0; i<costs.size(); i++){
+        if(edges == n-1) break;
+        
+        if(Find(costs[i][0]) != Find(costs[i][1])){
+            Union(costs[i][0], costs[i][1]);
+            answer += costs[i][2];
+            edges++;
+        }
+    }
+    
+    
+    return answer;
+}
