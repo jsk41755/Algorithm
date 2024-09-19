987a0f2017a55a1b264f9431ce2994667d6ace49
정승규 <43807509+jsk41755@users.noreply.github.com>
2024-09-20 01:12:43 +0900

    [level 3] Title: 네트워크, Time: 0.06 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -1,20 +1,19 @@
 #include <string>
 #include <vector>
-#include <iostream>
 #include <algorithm>
 
 using namespace std;
 
 vector<int> arr;
 
-int Find(int i){
+int find(int i){
     if(arr[i] == i) return i;
-    else return arr[i] = Find(arr[i]);
+    else return arr[i] = find(arr[i]);
 }
 
 void Union(int a, int b){
-    int aRoot = Find(a);
-    int bRoot = Find(b);
+    int aRoot = find(a);
+    int bRoot = find(b);
     
     arr[aRoot] = bRoot;
 }
@@ -22,7 +21,7 @@ void Union(int a, int b){
 int solution(int n, vector<vector<int>> computers) {
     int answer = 0;
     
-    arr.resize(n+1);
+    arr.resize(n+1, 0);
     
     for(int i=1; i<=n; i++){
         arr[i] = i;
@@ -37,19 +36,17 @@ int solution(int n, vector<vector<int>> computers) {
     }
     
     for(int i=1; i<=n; i++){
-        arr[i] = Find(arr[i]);    
+        arr[i] = find(arr[i]);    
     }
     
     
-    
     sort(arr.begin(), arr.end());
-    
     arr.erase(unique(arr.begin(), arr.end()), arr.end());
-    
     
     for(int i=1; i<arr.size(); i++){
         answer++;
     }
+    
     
     return answer;
 }
