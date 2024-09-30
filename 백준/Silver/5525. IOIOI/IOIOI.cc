cb12f01ab6d62bc91c04c31a2a742be881629c9c
정승규 <43807509+jsk41755@users.noreply.github.com>
2024-09-30 19:44:17 +0900

    [Silver I] Title: IOIOI, Time: 8 ms, Memory: 7588 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.cc
+++ 백준/Silver/5525. IOIOI/IOIOI.cc
@@ -4,6 +4,7 @@
 
 using namespace std;
 
+int arr[1000001];
 
 int main() {
 	ios::sync_with_stdio(0);
@@ -12,42 +13,44 @@ int main() {
 
 	int n, m;
 	cin >> n >> m;
-
-	bool flag= false;
 	
 	string s;
 	int cnt = 0;
 	int ans = 0;
 	cin >> s;
 
-	vector<int> v;
+	bool flag = false;
 
-	for (int i = 0; i < m; i++) {
-		if (s[i] == 'I') v.push_back(i);
+	if (s[0] == 'I') {
+		arr[0] = 1;
+		flag = true;
+	}
+	
+	for (int i = 1; i < m; i++) {
+		if (!flag && s[i] == 'I') {
+			arr[i] = arr[i - 1] + 1;
+			flag = true;
+		}
+		else if (flag && s[i] == 'O') {
+			arr[i] = arr[i - 1] + 1;
+			flag = false;
+		}
+		else if(s[i] == 'I' && flag) {
+			arr[i] = 1;
+		}
+		else {
+			arr[i] = 0;
+			flag = false;
+		}
 	}
 
-	for (auto a : v) {
-		for (int i = a; i < a + n + n + 1 && i<m; i++) {
-			if (s[i] == 'I' && !flag) {
-				cnt++;
-				flag = true;
-			}
-			else if (s[i] == 'O' && flag) {
-				cnt++;
-				flag = false;
-			}
-			else {
-				break;
-			}
+	for (int i = 0; i < m - n - n; i++) {
+		if (arr[i] % 2 == 1 && arr[i + n + n] - arr[i] == n + n) {
+			ans++;
 		}
-
-		if (cnt == n + n + 1) ans++;
-		cnt = 0;
-		flag = false;
 	}
 
 	cout << ans;
-
 
 	return 0;
 }
