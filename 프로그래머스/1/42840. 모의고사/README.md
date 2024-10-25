fc7b79a5bd9ac91db6bcac7d87aa7c1f397ae5fc
정승규 <43807509+jsk41755@users.noreply.github.com>
2024-10-25 22:29:23 +0900

    [level 1] Title: 모의고사, Time: 2.89 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
 
 ### 성능 요약
 
-메모리: 76.3 MB, 시간: 3.61 ms
+메모리: 78.5 MB, 시간: 2.89 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 03일 17:33:26
+2024년 10월 25일 22:29:21
 
 ### 문제 설명
 
--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -2,39 +2,53 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] answers) {
-        int[] a = {1,2,3,4,5};
-        int[] b = {2,1,2,3,2,4,2,5};
-        int[] c = {3,3, 1,1, 2, 2, 4, 4, 5, 5};
+        int[] a = {1, 2, 3, 4, 5};
+        int[] b = {2, 1, 2, 3, 2, 4, 2, 5,};
+        int[] c = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
         
-        int[] ans = {0, 0, 0};
+        int aa = 0, bb = 0, cc = 0;
+        
+        int m = 0;
         
         for(int i=0; i<answers.length; i++){
-            if(answers[i] == a[i%a.length]){
-                ans[0]++;
-            }
-            if(answers[i] == b[i%b.length]){
-                ans[1]++;
-            }
-            if(answers[i] == c[i%c.length]){
-                ans[2]++;
-            }
+            if(answers[i] == a[i%a.length]) aa++;
+            if(answers[i] == b[i%b.length]) bb++;
+            if(answers[i] == c[i%c.length]) cc++;
+            
+            m = Math.max(m, aa);
+            m = Math.max(m, bb);
+            m = Math.max(m, cc);
+            
         }
         
-        int max = -1;
-        for(int i=0; i<ans.length; i++){
-            if(max < ans[i]){
-                max = ans[i];
-            }
+        int cnt = 0;
+        
+        if(aa == m){
+            cnt++;
         }
         
-        ArrayList<Integer> answer = new ArrayList<>();
-        
-        for(int i=0; i<ans.length; i++){
-            if(max == ans[i]){
-                answer.add(i+1);
-            }
+        if(bb == m){
+            cnt++;
         }
         
-        return answer.stream().mapToInt(i -> i).toArray();
+        if(cc == m){
+            cnt++;
+        }
+        
+        int[] answer = new int[cnt];
+        cnt = 0;
+        if(aa == m){
+            answer[cnt++] = 1;
+        }
+        
+        if(bb == m){
+            answer[cnt++] = 2;
+        }
+        
+        if(cc == m){
+            answer[cnt++] = 3;
+        }
+        
+        return answer;
     }
 }
